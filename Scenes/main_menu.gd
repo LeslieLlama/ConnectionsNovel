70b15ce5d21046509f6091ff84b887580f48b2ef
LeslieLlama <leslieundinecrooks@gmail.com>
2025-07-27 16:01:26 +1200

    Main Menu Bugfix
    Fixed the bug where the main menu styling wouldn't disappear when the game starts.

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -4,11 +4,11 @@ extends Control
 const MAIN_SCENE = preload("res://Scenes/main_scene.tscn")
 
 func _on_start_button_pressed() -> void:
-	start_button.hide()
+	$MainMenuFlavors.hide()
 	var main_scene = MAIN_SCENE.instantiate()
 	add_child(main_scene)
 	
 	await main_scene.game_completed
 	
 	main_scene.queue_free()
-	start_button.show()
+	$MainMenuFlavors.show()
