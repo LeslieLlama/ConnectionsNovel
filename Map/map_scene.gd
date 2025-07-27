2446d9075c9f835fe7d94a0c0d647b50da76ca65
LeslieLlama <leslieundinecrooks@gmail.com>
2025-07-27 14:48:32 +1200

    Added aquarium Map
    Added aquarium map, button style and new backgrounds

--- Map/map_scene.gd
+++ Map/map_scene.gd
@@ -1,6 +1,6 @@
 extends Control
 @onready var map_nodes: Control = %MapNodes
-
+var nav_button = preload("res://Themes/navigation_button.tscn")
 signal button_pressed(Encounter)
 
 func get_map_node(location: Location) -> MapLocation:
@@ -13,11 +13,11 @@ func display_encounter_choices(time_slot: TimeSlot):
 	for encounter in time_slot.encounters:
 		var map_node = get_map_node(encounter.location)
 		
-		var button = Button.new()
-		button.text = "Connect with {0} in the {1}".format([
+		var button = nav_button.instantiate()
+		button.text = "Connect with {0}".format([
 			encounter.character.character_name,
 			encounter.location.location_name])
-
+		button.visible = true
 		add_child(button)
 		button.global_position = map_node.global_position-button.size/2
 		
